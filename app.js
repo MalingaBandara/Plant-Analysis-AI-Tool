7e3ca29577c4d6f28f70b99cf3840cf5f2ed7b41
Malinga Bandara <malinga1021@gmail.com>
2025-09-10 16:19:28 +0530

    Import GoogleGenAI and config server

--- app.js
+++ app.js
@@ -6,3 +6,15 @@ const pdfkit = require('pdfkit'); // * Import PDFKit for generating PDF document
 const fs = require('fs'); // * Import Node.js File System (fs) module for working with files
 const fsPromises = fs.promises; // * Use the Promise-based version of fs (fs.promises) for async/await operations
 
+// * Import GoogleGenAI from the @google/genai package
+//   - This is used to interact with Google's Generative AI models
+import { GoogleGenAI } from "@google/genai";
+
+const app = express(); // * Initialize an Express application
+const PORT = process.env.PORT || 3000;
+
+
+//! Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
